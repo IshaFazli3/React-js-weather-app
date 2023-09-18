fc9fb80dc930229f03b76ec40f64c1cb54e7aa7b
Isha F <137927479+IshaFazli3@users.noreply.github.com>
2023-09-18 18:17:56 -0500

    Made a few corrections

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import logo from './logo.svg';
 import Weather from "./Weather";
 import "./App.css";
-import './App.css';
+
 
 export default function App() {
   return (
